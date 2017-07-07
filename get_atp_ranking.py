0713eeaf666b71fc18b6ec088a7b9f01f4b55d20
kingshark <kingshark8848@live.com>
2017-07-07 13:06:14 +1000

    finish dete grab and single date ranking grab

--- get_atp_ranking.py
+++ get_atp_ranking.py
@@ -1,9 +1,37 @@
 
 import requests
 import bs4
+import json
+from datetime import datetime
+
+
+def get_atp_dates(save_file=None):
+	date_list = []
+
+	response = requests.get('http://www.atpworldtour.com/en/rankings/singles')
+	soup = bs4.BeautifulSoup(response.text)
+
+	path = soup.select('#filterHolder > div > div > div')
+	path = path[0]
+	path = path.select('div > ul > li')
+
+	for li in path:
+		if (li.attrs.get('class')[0] != "dropdown-default-label"):
+			my_date = li.get_text().strip()
+			date_list.append(my_date)
+
+	date_list.sort(key=lambda x: datetime.strptime(x, '%Y.%m.%d'), reverse=True)
+
+	# store to json file
+	if (save_file):
+		with open(save_file, 'w') as fp:
+			json.dump(date_list, fp)
+
+	return date_list
+
 
 # v_date: format is yyyy-mm-dd (2000-01-10)
-def get_atp_data(v_date):
+def get_atp_ranking(v_date):
 	data = []
 
 	response = requests.get('http://www.atpworldtour.com/en/rankings/singles?rankDate='+v_date+'&rankRange=0-10')
@@ -20,8 +48,16 @@ def get_atp_data(v_date):
 
 		data.append({'key': player, 'country': country, 'player': player, 'score': score})
 
-	print(data)
+	return data
+
+
+
+def get_history_atp_rankings(since="2000-01-01", date_file=None, save_file=None):
+	pass
 
 
 if __name__ == '__main__':
-	get_atp_data('2000-01-10')
+
+	get_atp_dates('date.json')
+
+	# get_atp_ranking('2000-01-10')
